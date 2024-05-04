9912dc3b42853d5865217bdc7797c8fa27fb2cdb
SamMwenda <skmwenda47@gmail.com>
2024-05-04 19:07:49 +0300

    corrected env variables

--- actions/getOpenWeatherData.ts
+++ actions/getOpenWeatherData.ts
@@ -4,8 +4,8 @@ export const getOpenWeatherData = async ({ location }: { location: Location }) =
   if (!location) {
     throw new Error("Location is not defined");
   }
-  const URL = process.env.APP_URL;
-  const API_KEY = process.env.OPENWEATHER_API_KEY
+  const URL = process.env.NEXT_PUBLIC_APP_URL;
+  const API_KEY = process.env.NEXT_PUBLIC_OPENWEATHER_API_KEY
   const data = await fetch(`${URL}api/openweather?lat=${location.latitude}&lon=${location.longitude}&appid=${API_KEY}`);
 
   if (!data.ok) {
